27536125a69330435e9a47ec8104e02a7dddc470
noisonnoiton <noisonnoiton@gmail.com>
2023-06-08 15:39:16 +0900

    edit site name

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Kloudbank</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/kloudbank/kloudbank-nextra',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/kloudbank/kloudbank-nextra',
   footer: {
     text: 'Nextra Docs Template',
   },
